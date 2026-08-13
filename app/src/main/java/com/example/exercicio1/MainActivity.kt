2a71b066954d2bbfc6afd5a93ff6589fe3c79112
phietroaguilar <phietrosousa10@gmail.com>
2026-08-13 08:05:01 -0300

    primeira versão

---
+++ app/src/main/java/com/example/exercicio1/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.exercicio1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.exercicio1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val login = binding.loginInput.text.toString().trim()
+            val senha = binding.senhaInput.text.toString().trim()
+
+            if(login == "AlicePhietro" && senha == "1529"){
+
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("LOGIN_USUARIO", login)
+                startActivity(intent)
+                finish()
+
+            } else {
+                Toast.makeText(this, "Login e senha incorretos!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/exercicio1/MainActivity2.kt
@@ -0,0 +1,21 @@
+package com.example.exercicio1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.exercicio1.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val loginUsuario = intent.getStringExtra("LOGIN_USUARIO")?: "Desconhecido"
+        binding.txtusuario.text="Bem vindo, $loginUsuario!"
+    }
+}
